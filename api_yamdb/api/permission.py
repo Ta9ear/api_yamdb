9318bbc0c8686b7c7d492faff3f39f6c9052f278
Гусев Владислав <guseff.vladislaw2011@yandex.ru>
2022-09-11 21:31:56 +0300

    YaMDB6

--- api_yamdb/api/permission.py
+++ api_yamdb/api/permission.py
@@ -1,7 +1,18 @@
 from rest_framework import permissions
 
 
-class IsAdminOrReadOnly(permissions.BasePermission):
+class IsAdminModeratorAuthorOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if request.user.is_admin:
+            return request.user.role == 'admin'
+        if request.user.is_moderator:
+            return request.user.role == 'moderator'
+        if obj.author == request.user:
+            return request.user.role == 'author'
+        return False
 
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,31 +1,30 @@
 from django.shortcuts import get_object_or_404
 from reviews.models import Review, Title
 from rest_framework import viewsets
-from .permissions import IsAdminOrReadOnly
+from .permissions import IsAdminModeratorAuthorOrReadOnly
 from .serializers import ReviewSerializer, CommentSerializer
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
-    permission_classes = [IsAdminOrReadOnly]
+    permission_classes = [IsAdminModeratorAuthorOrReadOnly]
 
     def perform_create(self, serializer):
-        get_object_or_404(Title, pk=self.kwargs.get('Title_id'))
-        serializer.save(author=self.request.user)
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        serializer.save(author=self.request.user, title=title)
 
     def get_queryset(self):
-        title = get_object_or_404(Title, pk=self.kwargs.get('Title_id'))
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
         return title.reviews.all()
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [IsAdminOrReadOnly]
+    permission_classes = [IsAdminModeratorAuthorOrReadOnly]
 
     def perform_create(self, serializer):
-        title_id = self.kwargs.get('title_id')
         review_id = self.kwargs.get('review_id')
-        review = get_object_or_404(Review, pk=review_id, title=title_id)
+        review = get_object_or_404(Review, pk=review_id)
         serializer.save(author=self.request.user, review=review)
 
     def get_queryset(self):
