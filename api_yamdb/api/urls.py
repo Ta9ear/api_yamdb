a1a313c0a589f25e69548deb0c294641889e1995
Гусев Владислав <guseff.vladislaw2011@yandex.ru>
2022-09-12 17:59:54 +0300

    YaMDB

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,17 +3,17 @@ from rest_framework.routers import DefaultRouter
 
 from .views import ReviewViewSet, TitleViewSet, CommentViewSet
 
-router = DefaultRouter()
-router.register('titles', TitleViewSet, basename='titles')
-router.register(
+router_v1 = DefaultRouter()
+router_v1.register('titles', TitleViewSet, basename='titles')
+router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews',
     ReviewViewSet, basename='reviews'
 )
-router.register(
+router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentViewSet, basename='comments'
 )
 
 urlpatterns = [
-    path('v1/', include(router.urls)),
+    path('v1/', include(router_v1.urls)),
 ]
