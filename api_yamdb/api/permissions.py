aed38c9bed79bafdec43355067fbb7bc7eaec2ea
Ta9ear <Ta.9ear@gmail.com>
2022-09-19 13:58:06 +0300

    migrations

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -5,7 +5,7 @@ class IsAdminModeratorAuthorOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
-                or request.user.role == 'admin'
+                or request.user.is_admin
                 or request.user.role == 'moderator'
                 or obj.author == request.user)
 
@@ -20,5 +20,5 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         if request.user.is_authenticated:
-            return request.user.role == 'admin'
+            return request.user.is_admin
         return False
