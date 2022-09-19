28d7a25ad59ae5d8bdb662150e35e7aa650fafc6
Ta9ear <Ta.9ear@gmail.com>
2022-09-19 14:02:42 +0300

    last_fix

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -6,7 +6,7 @@ class IsAdminModeratorAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or request.user.is_admin
-                or request.user.role == 'moderator'
+                or request.user.is_moderator
                 or obj.author == request.user)
 
     def has_permission(self, request, view):
